81be6c2eb91b18bab5734267c54fdabcb498237e
Sebastian <bastivoelkl@gmail.com>
2023-09-02 13:45:23 -0700

    rec

--- linkedin-recruiter-assistant/src/App.js
+++ linkedin-recruiter-assistant/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import InputArea from './components/InputArea';
 import Recommendations from './components/Recommendations';
 
 function App() {
+  const [recommendations, setRecommendations] = useState('Your recommendations will appear here.');
+
   return (
     <div className="App">
-      <InputArea />
-      <Recommendations recommendations="Your recommendations will appear here." />
+      <InputArea setRecommendations={setRecommendations} />
+      <Recommendations recommendations={recommendations} />
     </div>
   );
 }
 
 export default App;
+
